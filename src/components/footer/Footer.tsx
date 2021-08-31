22a1cfac92331456d30a740a10245bd4861c46da
Valentin Porta <valentinporta93@gmail.com>
2021-08-31 11:56:22 -0300

    Added a map to the footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import styles from './footer.module.css'
+import styles from './footer.module.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Footer = () => {
     return (
         <div id='contacto' className={styles.footerContainer}>
-            <p className={styles.footerTitle}>Contactanos!</p>
+            <p className={styles.footerTitle}>Pasa por nuestra oficina...</p>
+            <MapContainer center={[-33.692148582048226, -59.64825188247482]} zoom={15} scrollWheelZoom={true}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[-33.692148582048226, -59.64825188247482]}>
+                    <Popup>
+                    Oficina Porto Truck SRL
+                    </Popup>
+                </Marker>
+            </MapContainer>
+            <p className={styles.footerTitle}>O contactanos!</p>
             <ul className={styles.footerList}>
                 <li className={styles.footerListItem}><p>Celular: 12345678910</p></li>
                 <li className={styles.footerListItem}><p>Nextel: 12345678910</p></li>
